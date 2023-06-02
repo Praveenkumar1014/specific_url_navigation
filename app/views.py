12d93dcd9b1d9d6646ad4f8ff5ffc65afd25c925
PraveenKumar1014 <praveenreddypogula14@gmail.com>
2023-06-02 19:21:31 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def manju(request):
+    return render(request,'manju.html')
+def althaf(request):
+    return render(request,'althaf.html')
